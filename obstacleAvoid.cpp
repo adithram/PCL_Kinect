44052fa96dfed52c0bf3eee7e7338cd7587e2e52
Adithya Ramanathan <adithram@umich.edu>
2017-04-08 20:00:32 -0400

    updating double includes

--- obstacleAvoid.cpp
+++ obstacleAvoid.cpp
@@ -1,23 +1,3 @@
-#include <iostream>
-#include <pcl/io/pcd_io.h>
-#include <pcl/point_types.h>
-#include <chrono>
-#include <thread>
-#include <tuple>
-#include <stdio.h>   
-#include <stdlib.h> 
-#include <cmath>
-#include <pcl/ModelCoefficients.h>
-#include <pcl/filters/extract_indices.h>
-#include <pcl/filters/voxel_grid.h>
-#include <pcl/features/normal_3d.h>
-#include <pcl/kdtree/kdtree.h>
-#include <pcl/sample_consensus/method_types.h>
-#include <pcl/sample_consensus/model_types.h>
-#include <pcl/segmentation/sac_segmentation.h>
-#include <pcl/segmentation/extract_clusters.h>
-#include <pcl/kdtree/kdtree_flann.h>
-#include <pcl/surface/mls.h>
 #include "pc_background_subtract.h"
 
 using namespace std;
